ad8392c7d3cb5e62e3d379e61fcda99a7d501622
yang <diddnjswns20@gmail.com>
2024-07-06 16:06:00 +0900

    [이코테] 2장 구현

---
+++ 이코테/2장/4_2.py
@@ -0,0 +1,17 @@
+# 실패, 다시 1
+
+# import sys
+
+# input = sys.stdin.readline
+# n = input().strip()
+# last_time = f'{n}5959'
+# count = 0
+
+# for i in range(0, int(last_time)):
+#     if('3' in str(i)):
+#          count+=1
+
+# print(count)
+
+
+
